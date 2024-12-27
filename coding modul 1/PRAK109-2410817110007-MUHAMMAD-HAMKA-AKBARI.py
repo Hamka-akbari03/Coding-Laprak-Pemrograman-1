6b418cbfc0348d90578b32413322e919c5ec2167
Hamka-akbari03 <hamkaakbari41@gmail.com>
2024-12-28 00:51:35 +0800

    Delete coding modul 1 directory

--- coding modul 1/PRAK109-2410817110007-MUHAMMAD-HAMKA-AKBARI.py
+++
@@ -1,9 +0,0 @@
-pasukan = 958730
-pahlawan = 5
-
-print(f"Jumlah pasukan yang dibawa Yu Zhong = {pasukan}")
-print(f"Jumlah pahlawan = {pahlawan}")
-
-dikalahkan = pasukan / pahlawan
-
-print(f"Jumlah Pasukan yang harus dikalahkan setiap pahlawan adalah {dikalahkan:.0f} pasukan")
